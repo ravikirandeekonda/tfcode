c5d64d1c4439f51db8e9cf62c594b939c4bc1712
Deekonda Ravikiran <deekonda.ravikiran@cloud4c.com>
2025-01-15 12:21:34 +0530

    added output file in ec2 module

--- dev/output.tf
+++ dev/output.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   description = "VPC ID"
   value       = module.vpc_module.vpc_id
 }
+
+output "instance-public-ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = module.ec2mod.instance_public_ip
+}
